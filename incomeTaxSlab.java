9ba3b4305212a163a9120267d5cc82c7e92c1d9f
yash tomar <tomary880@gmail.com>
2026-01-17 16:05:42 +0530

    added programs

--- incomeTaxSlab.java
+++ incomeTaxSlab.java
@@ -1,1 +1,34 @@
 
+
+import java.util.Scanner;
+
+public class percentCalculator{
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter your Annual Income(in lakhs):");
+    double annualIncome = input.nextDouble();
+
+    double tax = 0;
+    
+    if (annualIncome <= 2.5) {
+      System.out.println("You are not required to pay any income tax.");
+    }
+
+   else if (annualIncome > 2.5 && annualIncome <= 5) {
+      tax = 0.05 * (annualIncome - 2.5);
+      System.out.println("Your income tax is: " + tax + " lakhs");
+    }
+
+   else if (annualIncome > 5 && annualIncome <= 10) {
+      tax = 0.05 * (5 - 2.5) + 0.2 * (annualIncome - 5);
+      System.out.println("Your income tax is: " + tax + " lakhs");
+    }
+
+   else {
+      tax = 0.05 * (5 - 2.5) + 0.2 * (10 - 5) + 0.3 * (annualIncome - 10);
+      System.out.println("Your income tax is: " + tax + " lakhs");
+    
+   }
+  }
+}
