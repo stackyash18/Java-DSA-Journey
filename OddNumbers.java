3a70489f4f9d1729e80a8e96047c5aea8c3c9b82
yash tomar <tomary880@gmail.com>
2026-01-18 18:36:45 +0530

    added programs

---
+++ OddNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OddNumbers {
+  public static void main(String[] args) {
+
+    Scanner input =  new Scanner(System.in);
+    System.out.println("Enter a positive integer: ");
+    int n = input.nextInt();
+    for(int i = 1; i <= n; i++)
+    {
+      if(i % 2 !=0)
+      {
+        System.out.println(i);
+      }
+    }
+  }
+}
