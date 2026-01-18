f54eabea4dc0d21356719e4b84da23361cdfaaaa
yash tomar <tomary880@gmail.com>
2026-01-18 15:10:41 +0530

    added file

---
+++ findingTheLeapYear.java
@@ -0,0 +1,22 @@
+public class findingTheLeapYear {
+  public static void main(String[] args) {
+    int year = 2024;
+    boolean isLeapYear = false;
+
+    if (year % 4 == 0) {
+      if (year % 100 == 0) {
+        if (year % 400 == 0) {
+          isLeapYear = true;
+        }
+      } else {
+        isLeapYear = true;x
+      }
+    }
+
+    if (isLeapYear) {
+      System.out.println(year + " is a leap year.");
+    } else {
+      System.out.println(year + " is not a leap year.");
+    }
+  }
+}
