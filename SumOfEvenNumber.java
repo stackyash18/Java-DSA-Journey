316e3128cef9eb5f4664e8725961cd8d3f009606
yash tomar <tomary880@gmail.com>
2026-01-20 00:03:49 +0530

    Added the Programmes

---
+++ SumOfEvenNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumOfEvenNumber {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter a Positive Integer: ");
+
+    int n = input.nextInt();
+
+    int sum =0;
+
+    for(int i = 1; i <= n; i++)
+    {
+     if(i % 2 == 0)
+     {
+      sum += i;
+     }
+    }
+
+    System.out.println("Sum of even numbers from 1 to " + n + " is: " + sum);
+  }
+}
---
+++ TableOfnNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TableOfnNumber {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter a Number of which you want to print the Table:");
+    int n = input.nextInt();
+
+    for(int i = 1; i <= 10; i++)
+    {
+      System.out.println(n +  " x " + i + " = " + (n * i));
+    }
+  }
+}
