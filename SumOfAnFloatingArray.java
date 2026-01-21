89c4c19451011d5cc884e202f3b7c3fbea068319
yash tomar <tomary880@gmail.com>
2026-01-21 22:30:45 +0530

    Added The Program Files

---
+++ SumOfAnFloatingArray.java
@@ -0,0 +1,11 @@
+public class SumOfAnFloatingArray {
+  public static void main(String[] args) {
+    float[] array = {1.5f, 2.5f, 3.5f, 4.5f, 5.5f};
+    float sum = 0;
+    
+     for(float element: array) {
+      sum += element;
+     }
+     System.out.println("Sum of Floating Array: " + sum);
+  }
+}
