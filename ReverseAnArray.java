b463bc7a5a272a6d0e9292442f61de753d2c563a
yash tomar <tomary880@gmail.com>
2026-01-22 12:40:29 +0530

    Added the program files

--- ReverseAnArray.java
+++ ReverseAnArray.java
@@ -1,5 +1,18 @@
 public class ReverseAnArray {
   public static void main(String[] args) {
-  
+    int[] array = {1,2,3,4,5,6,7,8,9,10};
+    int mid = Math.floorDiv(array.length,2);
+
+    for(int i=0; i<mid; i++)
+    {
+      int temp = array[i];
+      array[i] = array[array.length - 1 - i];
+      array[array.length - 1 - i] = temp;
+       
+    }
+    for(int num:array)
+    {
+      System.out.print(num + " ");
+    }
   }
 }
