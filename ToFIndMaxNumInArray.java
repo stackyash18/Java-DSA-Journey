1ddfc9a300237b9f400093ce4e94b782cfcb8783
yash tomar <tomary880@gmail.com>
2026-01-22 12:59:20 +0530

    Added the program files

---
+++ ToFIndMaxNumInArray.java
@@ -0,0 +1,14 @@
+public class ToFIndMaxNumInArray {
+  public static void main(String[] args) {
+    int[]  array = {34, 67, 23, 89, 2, 90, 45, 78};
+    int maxNum = array[0];
+    for(int i = 0; i < array.length; i++)
+    {
+      if(array[i] > maxNum)
+      {
+        maxNum = array[i];
+      }
+    }
+    System.out.println("Maximum number in the array is: " + maxNum);
+  }
+}
---
+++ ToFindMinNumInArray.java
@@ -0,0 +1,14 @@
+public class ToFindMinNumInArray {
+  public static void main(String[] args) {
+    int[] array = {45,52,-4503,654,412,485,965,862};
+    int minNum = array[0];
+
+    for(int i = 0; i < array.length; i++)
+    {
+      if (array[i] < minNum) {
+        minNum = array[i];
+      }
+    }
+    System.out.println("Minimum number in the array is: " + minNum);
+  }
+}
