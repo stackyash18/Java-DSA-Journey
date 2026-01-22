2d50b4411449c0dbeafe55524898a144d0d76515
yash tomar <tomary880@gmail.com>
2026-01-22 13:15:46 +0530

    Added the program files

---
+++ ToCheckAnArrayIsSortedOrNot.java
@@ -0,0 +1,25 @@
+public class ToCheckAnArrayIsSortedOrNot {
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5, 15, 7, 8, 9, 10};
+    boolean isSorted = true;
+    
+    for(int i = 0; i<arr.length-1; i++)
+    {
+      if (arr[i] > arr[i+1]) {
+        isSorted = false;
+        break;
+      }
+    }
+    if(isSorted)
+    {
+      System.out.println("The array is sorted in ascending order.");
+    }
+    else
+    {
+      System.out.println("The array is not sorted in ascending order.");
+    }
+  }
+}
+
+
+
