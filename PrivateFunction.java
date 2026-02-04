6dc6fe12ce26e30c39264994222431deb4b7f9ef
yash tomar <tomary880@gmail.com>
2026-02-04 10:31:27 +0530

    added the program files

---
+++ PrivateFunction.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class PrivateFunction {
+
+   class MyEmployee
+  {
+    private int id;
+    private String name;
+
+
+    public String getName()
+    {
+      return name;
+    }
+    public void setName(String n)
+    {
+      name = n;
+    }
+  
+    public void setId(int i)
+    {
+      id = i;
+    }
+
+    public int getId()
+    {
+      return id;
+    }
+
+
+  }
+
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    PrivateFunction pf = new PrivateFunction();
+    MyEmployee Harry = pf.new MyEmployee();
+    String name = input.nextLine();
+    Harry.setName(name);
+    
+    int Id = input.nextInt();
+    Harry.setId(Id);
+
+    System.out.println("Name: " + Harry.getName());
+    System.out.println("Id: " + Harry.getId());
+
+  }
+
+ 
+ 
+
+}
