b48cee0ff82b211cb976c4006d170e8fac38a8e7
yash tomar <tomary880@gmail.com>
2026-02-04 11:20:24 +0530

    Added the program files

---
+++ CirclleUsingPrivateFunc.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class CirclleUsingPrivateFunc {
+
+    private double radius;
+    private double area;
+    private double circumference;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double r) {
+        radius = r;
+        calculateArea();          
+        calculateCircumference(); 
+    }
+
+    private void calculateArea() {
+        area = Math.PI * radius * radius;
+    }
+
+    private void calculateCircumference() {
+        circumference = 2 * Math.PI * radius;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getCircumference() {
+        return circumference;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        CirclleUsingPrivateFunc circle = new CirclleUsingPrivateFunc();
+
+        System.out.print("Enter radius: ");
+        double r = input.nextDouble();
+
+        circle.setRadius(r);
+
+        System.out.println("Radius: " + circle.getRadius());
+        System.out.println("Area: " + circle.getArea());
+        System.out.println("Circumference: " + circle.getCircumference());
+
+        input.close();
+    }
+}
