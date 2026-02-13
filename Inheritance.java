f2323d9b193ad0dbb359492c2f852e400886943c
yash tomar <tomary880gmail.com>
2026-02-13 21:59:50 +0530

    added the program files

---
+++ Inheritance.java
@@ -0,0 +1,41 @@
+ class Base
+ {
+  int x;
+
+  public int getX()
+  {
+    return x;
+  }
+  public void setX(int x)
+  {
+    System.out.println("This is the base class");
+    this.x = x;
+  }
+
+  public void printMe()
+  {
+    System.out.println("I am a constructor");
+  }
+ }
+
+ class Derived extends Base
+ {
+
+  int y;
+
+  public int getY()
+  {
+    return y;
+  }
+  public void setY(int y)
+  {
+    System.out.println("This is the derived class");
+    this.y = y;
+  }
+ }
+public class Inheritance {
+  public static void main(String[] args)
+  {
+    
+  }
+}
